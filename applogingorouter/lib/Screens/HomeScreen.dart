0b20ef55108c61f4f0d7f3f6aada82167612dffe
JoaquinBitran <joaspace07@gmail.com>
2024-06-02 18:32:22 -0300

    AppLoginRouter

--- applogingorouter/lib/Screens/HomeScreen.dart
+++ applogingorouter/lib/Screens/HomeScreen.dart
@@ -4,16 +4,32 @@ void main() {
   runApp(const HomeScreen());
 }
 
- class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key key}) : super(key: key);
+
+  final List<String> frutas = ['Manzana', 'Pera', 'Durazno', 'Uva', 'Tomate', 'Anana', 'Banana', 'Ciruela', 'Sandia', 'Frutilla'];
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Welcome User'),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Frutas'),
+      ),
+      body: ListView.builder(
+        itemCount: frutas.length,
+        itemBuilder: (context, index) {
+          return Column(
+            children: <Widget>[
+              ListTile(
+                title: Text(frutas[index]),
+                onTap: () {
+                 
+                },
+              ),
+              Divider(thickness: 3, color: Color.fromARGB(255, 81, 172, 70)),
+            ],
+          );
+        },
       ),
     );
   }
